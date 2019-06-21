5ca55f585d0c116407869a1a3f66a28177ed68d1
Simon Gomes <busy.s.simon@gmail.com>
2019-06-21 17:14:05 +0600

    thunk middleware applied to redux store, thunk-setup with the project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,28 +1,7 @@
 import { INITIAL_STORIES } from './../actions/actionTypes';
 import { ADD_STORY } from './../actions/actionTypes';
 
-const initialState = {
-  stories: [
-    {
-      title:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team'
-    },
-    {
-      title:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team'
-    },
-    {
-      title:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team'
-    },
-    {
-      title:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team'
-    }
-  ]
-};
-
-function rootReducer(state = initialState, action) {
+function rootReducer(state, action) {
   switch (action.type) {
     case INITIAL_STORIES:
       return Object.assign({}, state, { stories: state.stories });
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,37 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
+
+const middlewares = [thunk];
+
+const initialState = {
+  stories: [
+    {
+      title:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team'
+    },
+    {
+      title:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team'
+    },
+    {
+      title:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team'
+    },
+    {
+      title:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team'
+    }
+  ],
+  active_channel: 'hacker-news'
+};
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  storeEnhancers(applyMiddleware(...middlewares))
+);
 
 export default store;
