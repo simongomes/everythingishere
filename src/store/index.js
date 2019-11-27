73188103e9228be3468df7a31205022058558bdb
Simon Gomes <simon@webable.com.bd>
2019-11-27 14:42:03 +0600

    __REDUX_DEVTOOLS_EXTENSION__ omitted for production mode

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from '../reducers';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducers";
+import thunk from "redux-thunk";
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
 
@@ -8,16 +8,25 @@ const middlewares = [thunk];
 
 const initialState = {
   stories: [],
-  channel: 'Everything'
+  channel: "Everything"
 };
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  storeEnhancers(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+let store = null;
+
+process.env.NODE_ENV !== "production"
+  ? (store = createStore(
+      rootReducer,
+      initialState,
+      storeEnhancers(applyMiddleware(...middlewares))
+    ))
+  : (store = createStore(
+      rootReducer,
+      initialState,
+      storeEnhancers(
+        applyMiddleware(...middlewares),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    ));
 
 export default store;
