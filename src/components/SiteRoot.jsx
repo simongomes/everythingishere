d6c37d93fae5743eff01b13e6529b2da97d28032
Simon Gomes <busy.s.simon@gmail.com>
2019-12-14 19:32:23 +0600

    Google Analytics added

--- src/components/SiteRoot.jsx
+++ src/components/SiteRoot.jsx
@@ -1,18 +1,27 @@
-import React, { Component } from "react";
-import { Layout } from "antd";
-import Sidebar from "./Sidebar";
-import Content from "./Content";
+import React, { Component } from 'react';
+import { Layout } from 'antd';
+import Sidebar from './Sidebar';
+import Content from './Content';
+import { initGA, logPageView } from './../libs/googleAnalytics';
 
 class SiteRoot extends Component {
-    render() {
-        return (
-            <div>
-                <Layout>
-                    <Sidebar />
-                    <Content />
-                </Layout>
-            </div>
-        );
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
     }
+    logPageView();
+  }
+
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Sidebar />
+          <Content />
+        </Layout>
+      </div>
+    );
+  }
 }
 export default SiteRoot;
